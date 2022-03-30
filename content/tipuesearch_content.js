c461625e30d08fc3d0e824b0e1c08a0ba003980a
409231044 <irene971726@gmail.com>
2022-03-30 08:34:37 +0800

    NX1980

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n \n', 'tags': '', 'url': 'About.html'}, {'title': '小組分組影片', 'text': '參考影片: https://www.youtube.com \n 我被分配到第31-36的影片', 'tags': '', 'url': '小組分組影片.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n \n', 'tags': '', 'url': 'About.html'}, {'title': 'NX1980', 'text': '小組分組影片 \n 參考影片: https://www.youtube.com \n 我被分配到第31-36的影片', 'tags': '', 'url': 'NX1980.html'}]};
